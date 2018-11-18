6ae74e4fd3cdb892eb24a832fac34526d99e058d
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2018-11-18 12:47:38 -0200

    BUG FIX - exporting has_one relation

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_134758) do
+ActiveRecord::Schema.define(version: 2018_11_18_143958) do
+
+  create_table "cats", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cats_on_user_id"
+  end
 
   create_table "dogs", force: :cascade do |t|
     t.integer "user_id"
