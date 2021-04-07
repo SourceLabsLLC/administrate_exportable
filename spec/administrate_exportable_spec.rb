cb490902e837f5c36085e5701d032c5772372e6e
Dmitry Zuev <d.zuev@rambler-co.ru>
2021-04-07 18:50:52 +0300

    Use correct page param name

--- spec/administrate_exportable_spec.rb
+++ spec/administrate_exportable_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe AdministrateExportable do
   it "has a version number" do
     expect(AdministrateExportable::VERSION).not_to be nil
   end
+
+  it "has a PAGE_PARAM constant" do
+    expect(AdministrateExportable::PAGE_PARAM).not_to be nil
+  end
 end
