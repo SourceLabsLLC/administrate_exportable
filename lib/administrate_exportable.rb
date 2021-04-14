6bd08c6de05ba9616325d017ff2fa793e85a2f02
Dmitry Zuev <mail@dzuev.ru>
2021-04-14 05:00:14 +0300

    Use correct page param name (#36)

--- lib/administrate_exportable.rb
+++ lib/administrate_exportable.rb
@@ -1,8 +1,11 @@
 require "administrate_exportable/version"
 require "administrate_exportable/exporter_service"
 require "administrate_exportable/exporter"
+require "administrate/version"
 
 module AdministrateExportable
+  PAGE_PARAM = Gem::Version.create(Administrate::VERSION) >= Gem::Version.create('0.15.0') ? '_page' : 'page'
+
   class Engine < ::Rails::Engine
   end
 end
